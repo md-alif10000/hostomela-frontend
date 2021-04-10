15c7213f1481c337cced81039b25d192d1d1afc9
Md Alif <md.alif10000@gmail.com>
2021-04-10 20:40:54 +0600

    changed base url

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,5 +1,5 @@
-export const api = "http://localhost:2000/api";
-export const domain = "http://localhost:2000";
+export const api = "https://hostomela-backend.herokuapp.com/api";
+export const domain = "https://hostomela-backend.herokuapp.com/";
 export const generatePublicUrl = (fileName) => {
-	return `http://localhost:2000/public/${fileName}`;
+	return `https://hostomela-backend.herokuapp.com/public/${fileName}`;
 };
