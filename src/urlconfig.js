c78a8d7bb23bedd19af6d78ef6dc2a95c5ab18db
md_alif <md.alif10000@gmail.com>
2021-05-05 19:51:46 +0600

    ..

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,16 +1,16 @@
-// export const api = "https://hostomela-backend.herokuapp.com/api";
+export const api = "https://hostomela-backend.herokuapp.com/api";
 
-// export const domain = "https://hostomela-backend.herokuapp.com";
+export const domain = "https://hostomela-backend.herokuapp.com";
 
-// export const generatePublicUrl = (fileName) => {
-// 	return `https://hostomela-backend.herokuapp.com/public/${fileName}`;
-// };
-
-
-export const siteUrl="https://hostomela.herokuapp.com"
-
-export const api = "http://localhost:2000/api";
-export const domain = "http://localhost:2000";
 export const generatePublicUrl = (fileName) => {
-	return `http://localhost:2000/public/${fileName}` ;
-}
+	return `https://hostomela-backend.herokuapp.com/public/${fileName}`;
+};
+
+
+// export const siteUrl="https://hostomela.herokuapp.com"
+
+// export const api = "http://localhost:2000/api";
+// export const domain = "http://localhost:2000";
+// export const generatePublicUrl = (fileName) => {
+// 	return `http://localhost:2000/public/${fileName}` ;
+// }
